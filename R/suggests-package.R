2e58ab46c18333ed6f9364f179e7696ce236fc5f
owenjonesuob <21007837+owenjonesuob@users.noreply.github.com>
2022-07-30 17:26:46 +0100

    Add package-level documentation

---
+++ R/suggests-package.R
@@ -0,0 +1,8 @@
+#' @name suggests
+#' @description By adding dependencies to the "Suggests" field of a package's
+#'   DESCRIPTION file, and then declaring that they are needed within any
+#'   dependent functionality, it is often possible to significantly reduce the
+#'   number of "hard" dependencies required by a package.
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
