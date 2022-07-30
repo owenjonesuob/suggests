59f1f36e3a0173d961241fb7b882c9856a79df6b
owenjonesuob <21007837+owenjonesuob@users.noreply.github.com>
2022-07-30 17:28:31 +0100

    Rename prompt parameter to ask

--- R/need.R
+++ R/need.R
@@ -1,17 +1,17 @@
 
-#' Declare that a suggested package is required
+#' Declare that a package is needed
 #'
 #' Declare that one or more packages are required by subsequent functionality,
-#' and either prompt the user to install them, or exit with an informative
-#' error message.
+#' and if they're missing, either prompt the user to install them, or exit with
+#' an informative error message.
 #'
 #' @param ... Names of required packages, as character strings.
-#' @param prompt Whether to give the user the option of installing the required
+#' @param ask Whether to give the user the option of installing the required
 #'   packages immediately.
 #'
 #' @return Invisibly, the names of any packages which were installed.
 #'
-#' @examples
+#' @examples \dontrun{
 #'
 #'   read_data <- function(path, clean_names = FALSE) {
 #'
@@ -25,26 +25,29 @@
 #'       output <- janitor::clean_names(output)
 #'
 #'     output
+#'   }
 #'  }
 #'
 #' @export
 
-need <- function(..., prompt = interactive()) {
+need <- function(..., ask = interactive()) {
 
   pkgs <- sort(unlist(c(...)))
 
   loaded <- vapply(pkgs, requireNamespace, quietly = TRUE, FUN.VALUE = logical(1))
 
-  if (!all(loaded))
-    message(
-      "Additional packages are required to use this function:",
-      "\n  ",
-      paste0(pkgs[!loaded], collapse = ", ")
-    )
+  # Nothing to do if all packages are already present!
+  if (all(loaded))
+    return(invisible(character()))
+
+  message(
+    "Additional packages are required to use this function: ",
+    paste0(pkgs[!loaded], collapse = ", "),
+    "\n"
+  )
 
 
-
-  if (isTRUE(prompt))
+  if (isTRUE(ask))
     choice <- utils::menu(
       title = "Would you like to install these packages now?",
       choices = c("Yes", "No")
@@ -52,7 +55,7 @@ need <- function(..., prompt = interactive()) {
 
 
 
-  if (!isTRUE(prompt) || choice != 1)
+  if (!isTRUE(ask) || choice != 1)
     stop(
       "Please install the following packages to use this function: ",
       "\n\n  install.packages(c(",
@@ -62,6 +65,6 @@ need <- function(..., prompt = interactive()) {
     )
 
 
-  install.packages(pkgs[!loaded])
+  utils::install.packages(pkgs[!loaded])
   invisible(pkgs[!loaded])
 }
